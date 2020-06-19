6d2062b801c714ace8c64c446081015effc11ffc
Bogdan Valentin <63101897+BogdanValentin@users.noreply.github.com>
2020-06-19 18:34:49 +0300

    Add files via upload

---
+++ gta5.cmd
@@ -0,0 +1,7 @@
+taskkill /F /IM GTA5.exe /T
+taskkill /F /IM Launcher.exe /T
+taskkill /F /IM PlayGTAV.exe /T
+taskkill /F /IM RockstarService.exe /T
+taskkill /F /IM SocialClubHelper.exe /T
+taskkill /F /IM RockstarService.exe /T
+taskkill /F /IM RockstarService.exe /T
